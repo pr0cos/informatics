b468642ad609ce0a20b2fe80d9145f3bfbf38f99
pr0cos <fedotglazko17@gmail.com>
2023-03-11 10:58:23 +0300

    deixtra

--- src/Board.java
+++ src/Board.java
@@ -2,7 +2,9 @@ import java.awt.*;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Rectangle2D;
+import java.lang.annotation.Retention;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 
 public class Board{
@@ -17,6 +19,8 @@ public class Board{
     int y_direction;
     int dx;
     int dy;
+    Rectangle last_room;
+    ArrayList<Enemy> enemies;
     ArrayList<Rectangle> rooms;
     ArrayList<PlayerBullet> playerBullets;
     ArrayList<ArrayList<Integer>> graph;
@@ -29,8 +33,9 @@ public class Board{
         this.cellSize = cellSize;
         x_direction = 0;
         y_direction = 0;
-        dx = 2;
-        dy = 2;
+        dx = 5;
+        dy = 5;
+        enemies = new ArrayList<>();
         rooms = new ArrayList<>();
         playerBullets = new ArrayList<>();
         board = new ArrayList<>();
@@ -47,6 +52,7 @@ public class Board{
                 graph.get(i).add(0);
             }
         }
+        last_room = null;
         generate();
     }
     void generate(){
@@ -62,7 +68,9 @@ public class Board{
                     }
                 }
             }
-            rooms.add(new Rectangle(1, 1, 10, 10));
+            Rectangle room = new Rectangle(1, 1, 10, 10);
+            rooms.add(room);
+            enemies.add(new Enemy(5 * cellSize, 5 * cellSize, 10, 10, room));
             this.paintRectangle(new Rectangle(11, 5, 5, 2), new Color(86, 15, 15));
         }else if(n == 2){
             for(int i = 0; i < start.length; i++){
@@ -72,7 +80,8 @@ public class Board{
                     }
                 }
             }
-            rooms.add(new Rectangle(1, 16, 10, 10));
+            Rectangle room = new Rectangle(1, 16, 10, 10);
+            rooms.add(room);
             this.paintRectangle(new Rectangle(11, 20, 5, 2), new Color(86, 15, 15));
         }else if(n == 3){
             for(int i = 0; i < start.length; i++){
@@ -82,7 +91,8 @@ public class Board{
                     }
                 }
             }
-            rooms.add(new Rectangle(1, 31, 10, 10));
+            Rectangle room = new Rectangle(1, 31, 10, 10);
+            rooms.add(room);
             this.paintRectangle(new Rectangle(11, 35, 5, 2), new Color(86, 15, 15));
         }else if(n == 4){
             for(int i = 0; i < start.length; i++){
@@ -92,7 +102,8 @@ public class Board{
                     }
                 }
             }
-            rooms.add(new Rectangle(31, 1, 10, 10));
+            Rectangle room = new Rectangle(31, 1, 10, 10);
+            rooms.add(room);
             this.paintRectangle(new Rectangle(26, 5, 5, 2), new Color(86, 15, 15));
         }else if(n == 5){
             for(int i = 0; i < start.length; i++){
@@ -102,7 +113,8 @@ public class Board{
                     }
                 }
             }
-            rooms.add(new Rectangle(31, 16, 10, 10));
+            Rectangle room = new Rectangle(31, 16, 10, 10);
+            rooms.add(room);
             this.paintRectangle(new Rectangle(26, 20, 5, 2), new Color(86, 15, 15));
         }else if(n == 6){
             for(int i = 0; i < start.length; i++){
@@ -112,7 +124,8 @@ public class Board{
                     }
                 }
             }
-            rooms.add(new Rectangle(31, 31, 10, 10));
+            Rectangle room = new Rectangle(31, 31, 10, 10);
+            rooms.add(room);
             this.paintRectangle(new Rectangle(26, 35, 5, 2), new Color(86, 15, 15));
         }
         String[] room1 = new Rooms().enemy_room();
@@ -146,7 +159,78 @@ public class Board{
         rooms.add(new Rectangle(16, 31, 10, 10));
     }
 
+    void update_enemies(Player player){
+        Rectangle room = get_room_screen(player.x, player.y);
+        if(room == null){
+            return;
+        }
+        Cell player_cell = get_cell_screen(player.x, player.y);
+        ArrayList<ArrayList<Integer>> paths = shortestPath((player_cell.x - room.x - 1) + (player_cell.y - room.y - 1) * 10);
+        for(Enemy enemy : enemies){
+            Cell enemy_cell = get_cell_screen((int) enemy.x, (int) enemy.y);
+            System.out.println(paths.get((enemy_cell.x - room.x - 1) + (enemy_cell.y - room.y - 1) * 10));
+        }
+    }
+
+    public ArrayList<ArrayList<Integer>> shortestPath(int i) {
+        double inf = 1000000000.0;
+        ArrayList<Double> d = new ArrayList<Double>();
+        ArrayList<ArrayList<Integer>> p = new ArrayList<>();
+        ArrayList<Integer> A = new ArrayList<Integer>();
+        ArrayList<Integer> B = new ArrayList<Integer>();
+        for (int k = 0; k < graph.size(); k++) {
+            d.add(0.0);
+            p.add(new ArrayList<Integer>());
+            if (k == i) {
+                d.set(k, 0.0);
+                ArrayList<Integer> a = new ArrayList<Integer>();
+                a.add(k);
+                p.set(k, a);
+                B.add(i);
+            } else {
+                d.set(k, inf);
+                B.add(k);
+            }
+        }
+        while (B.size() > 0) {
+            int V = 0;
+            double d_v = inf + 1;
+            if (B.size() > 1) {
+                for (int v : B) {
+                    if (d.get(v) < d_v) {
+                        V = v;
+                        d_v = d.get(v);
+                    }
+                }
+            } else {
+                V = B.get(0);
+                d_v = d.get(V);
+            }
+            for (int U = 0; U < graph.size(); U++) {
+                double a = graph.get(V).get(U);
+                if (a > 0) {
+                    if (d.get(U) < d.get(V) + graph.get(V).get(U)) {
+                        d.set(U, d.get(U));
+                    } else {
+                        d.set(U, d.get(V) + graph.get(V).get(U));
+                        ArrayList<Integer> c = (ArrayList<Integer>) p.get(V).clone();
+                        c.add(U);
+                        p.set(U, c);
+                    }
+                }
+            }
+            A.add(V);
+            B.remove((Integer) V);
+        }
+        return p;
+    }
+
+
+
     void make_graph(Rectangle room){
+        if(room == null || room == last_room){
+            return;
+        }
         for(int i = 0; i < room.height; i++){
             for(int j = 0; j < room.width; j++){
                 if(board.get(i + room.y).get(room.x + j).status){
@@ -165,7 +249,11 @@ public class Board{
                 }
             }
         }
+        System.out.println(graph);
+        last_room = room;
     }
+
+
     void paint(Graphics g){
         for(int i = 0; i < height; i++){
             for(int j = 0; j < width; j++){
@@ -174,6 +262,9 @@ public class Board{
         }
         for(PlayerBullet bullet : playerBullets){
             bullet.paint(g);
+        }
+        for(Enemy enemy : enemies){
+            enemy.paint(g);
         }
     }
     void paintRectangle(Rectangle rect, Color c){
@@ -197,6 +288,7 @@ public class Board{
         this.x += x_direction * dx;
         this.y += y_direction * dy;
         ArrayList<PlayerBullet> delete_player_bullets = new ArrayList<>();
+        ArrayList<Enemy> delete_enemies = new ArrayList<>();
         for(PlayerBullet bullet : playerBullets){
             bullet.x += x_direction * dx;
             bullet.y += y_direction * dy;
@@ -204,17 +296,49 @@ public class Board{
             if(!get_cell_screen((int)bullet.x, (int)bullet.y).status){
                 delete_player_bullets.add(bullet);
             }
+            for(Enemy enemy : enemies){
+                Line2D l = new Line2D.Float((int) bullet.x, (int)bullet.y, (int) (bullet.x + 2 * bullet.dx), (int) (bullet.y + 2 * bullet.dy));
+                Rectangle2D r1 = new Rectangle2D.Float((int)enemy.x, (int)enemy.y, enemy.getSize(), enemy.getSize());
+                System.out.println(enemy.getSize());
+                if(l.intersects(r1)){
+                    delete_player_bullets.add(bullet);
+                    enemy.hp -= bullet.damage;
+                }
+            }
         }
+        for(Enemy enemy : enemies){
+            enemy.update(x_direction * dx, y_direction * dy); // changing screen coordinates
+            Rectangle room = get_room_screen(player.x, player.y);
+            Rectangle2D player_rectangle = new Rectangle2D.Float(player.x, player.y, player.size, player.size);
+            Rectangle2D enemy_rectangle = new Rectangle2D.Float((float)enemy.x, (float)enemy.y, enemy.size, enemy.size);
+            if(player_rectangle.intersects(enemy_rectangle)){
+                player.damage(enemy.damage);
+            }
+            if(enemy.hp <= 0){
+                delete_enemies.add(enemy);
+            }
+        }
+        update_enemies(player);
         for(PlayerBullet bullet : delete_player_bullets){
             playerBullets.remove(bullet);
         }
+        for(Enemy enemy : delete_enemies){
+            enemies.remove(enemy);
+        }
         make_graph(get_room_screen(player.x, player.y));
-        System.out.println(graph);
     }
 
     public void bug_update(int x_d, int y_d){
         x += x_d * (dx);
         y += y_d * (dy);
+        for(Enemy enemy : enemies){
+            enemy.x += x_d * dx;
+            enemy.y += y_d * dy;
+        }
+        for(PlayerBullet bullet : playerBullets){
+            bullet.x += x_d * dx;
+            bullet.y += y_d * dy;
+        }
     }
     public Cell get_cell_world(int x, int y){
         int cell_x = x / cellSize;
---
+++ src/Enemy.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+
+public class Enemy {
+
+    double x;
+    double y;
+    int hp;
+    int damage;
+    int size;
+    Rectangle room;
+
+    public Enemy(double x, double y, int hp, int damage, Rectangle room) {
+        this.x = x;
+        this.y = y;
+        this.hp = hp;
+        this.damage = damage;
+        this.room = room;
+        size = 30;
+    }
+
+    public void update(double dx, double dy){
+        x += dx;
+        y += dy;
+    }
+
+    public void paint(Graphics g){
+        g.setColor(Color.YELLOW);
+        g.fillRect((int)x, (int)y, size, size);
+    }
+
+    public int getSize() {
+        return size;
+    }
+}
